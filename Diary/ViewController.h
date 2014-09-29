38dc8189e2ac32cf816f48f36a761afbe700485f
Ranjay Salmon <ranjy23@yahoo.com>
2014-09-28 20:02:42 -0400

    Initial Commit

---
+++ Diary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Diary
+//
+//  Created by Ranjay Salmon on 9/28/14.
+//  Copyright (c) 2014 Ranjay Salmon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
